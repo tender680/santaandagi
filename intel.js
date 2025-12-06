8b1851c9992e73d3027a3f158d2ddc1875ff10da
tender680 <114924190+tender680@users.noreply.github.com>
2025-12-07 00:11:39 +0100

    Update intel.js

--- intel.js
+++ intel.js
@@ -1,11 +1,14 @@
+let val = 0;
 document.addEventListener("DOMContentLoaded", () => {
     const maintext = document.getElementById("maintext");
-
+    
+    commpteur = document.getElementById("compteur")
     maintext.addEventListener("click", (e) => {
         // Jouer le son
         const beep = new Audio('santaandagi.mp3');
         beep.play().catch(err => console.error(err));
-
+        val+= 1;
+        commpteur.textContent = val;
         // Créer l'image
         const newImg = document.createElement('img');
         newImg.src = 'osaka.gif'; // ton image
