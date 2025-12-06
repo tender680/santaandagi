bcbf5ae7042d8d82462ba1dd89dc911967a578c3
tender680 <114924190+tender680@users.noreply.github.com>
2025-12-06 23:43:43 +0100

    Add files via upload

---
+++ intel.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const maintext = document.getElementById("maintext");
+
+    maintext.addEventListener("click", (e) => {
+        // Jouer le son
+        const beep = new Audio('santaandagi.mp3');
+        beep.play().catch(err => console.error(err));
+
+        // Créer l'image
+        const newImg = document.createElement('img');
+        newImg.src = 'osaka.gif'; // ton image
+        newImg.classList.add('flying-img');
+
+            // Position aléatoire dans la fenêtre
+        const maxX = window.innerWidth - 10; // largeur de l'image
+        const maxY = window.innerHeight - 10; 
+        newImg.style.left = `${Math.random() * maxX}px`;
+        newImg.style.top = `${Math.random() * maxY}px`;
+
+        // Ajouter au body
+        document.body.appendChild(newImg);
+
+        // Supprimer l'image après la fin de l'animation (2s ici)
+        setTimeout(() => {
+            newImg.remove();
+        }, 2000);
+    });
+});
